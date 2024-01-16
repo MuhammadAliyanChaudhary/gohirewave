e0cc7461d923e506c98a812a9454418fb047d65c
MuhammadAliyanChaudhary <aliyanch03055@gmail.com>
2024-01-16 10:43:49 +0500

    New files added for bottom navigation activity

---
+++ app/src/main/java/com/gohirewave/jobsapp/activities/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.gohirewave.jobsapp.activities.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
